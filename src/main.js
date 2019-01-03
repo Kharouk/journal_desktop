ae947dc532da91f2cd3ea462e2b7280cf9cb5c36
Alex Kharouk <akharoukpp@gmail.com>
2019-01-03 17:12:30 +0000

    Add menu to the application.

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 // Modules to control application life and create native browser window
-const { app, BrowserWindow } = require("electron");
+const { app, BrowserWindow, Menu } = require("electron");
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
@@ -11,6 +11,16 @@ function createWindow() {
 
   // and load the index.html of the app.
   mainWindow.loadURL("http://localhost:3000");
+
+  // Replace our top default menu with a template we created.
+  const template = [
+    {
+      label: "edit",
+      submenu: [{ role: "copy" }]
+    }
+  ];
+  const menu = Menu.buildFromTemplate(template);
+  Menu.setApplicationMenu(menu);
 
   // Open the DevTools.
   // mainWindow.webContents.openDevTools()
